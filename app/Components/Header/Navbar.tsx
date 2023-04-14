a5a490a0776d3727abc9ad1d7670c6d1adc80854
Owais_Gujjar909 <107211993+MOwais-gujjar@users.noreply.github.com>
2023-04-15 00:35:32 +0500

    first commit

--- app/Components/Header/Navbar.tsx
+++ app/Components/Header/Navbar.tsx
@@ -1,9 +1,28 @@
 import React from "react";
 
 const Navbar = () => {
+  const nav = [
+    { name: "Home", ulLink: "/" },
+    { name: "Projects", ulLink: "/" },
+    { name: "Skills", ulLink: "/" },
+    { name: "Certificates", ulLink: "/" },
+  ];
   return (
     <>
-      <div>Nav</div>
+      <header className=" py-5 ">
+        <div className="flex justify-between item-center">
+          {nav.map((l) => (
+            <ul key={l.ulLink}>
+              <li>{l.name}</li>
+            </ul>
+          ))}
+          <div>logo</div>
+          <div>
+            <div>icon</div>
+            <button>Resume</button>
+          </div>
+        </div>
+      </header>
     </>
   );
 };
